ac2663859ce0abfb448fb5145f2b1f6a6babc304
mima <mima@computas.com>
2022-01-11 19:52:53 +0100

    interfaces, enums, and mock data added

--- app.ts
+++ app.ts
@@ -1,8 +1,15 @@
 import express from 'express';
+import { todos } from "./data"
 
 const app = express();
 const port = 5000;
 
+app.get('/api', function (req, res) {
+    res.send('Sup, ima express API');
+});
+
 app.listen(port, () =>
-    {console.log(`app running on port ${port}`)}
-)
+    {console.log(`app running on port ${port}`)
+        console.log(todos)
+    }
+);
---
+++ enums/EStatuses.ts
@@ -0,0 +1,6 @@
+export enum EStatuses {
+    ASSIGNED = "Assigned",
+    IN_PROGRESS = "In Progress",
+    STUCK = "stuck",
+    DONE = "done",
+}
